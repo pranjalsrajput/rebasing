c837f3f1940e6b899706d63dc3be72b7963de014
Pranjal <pranjalsrajput@gmail.com>
2021-10-07 10:35:08 +0200

    push in test branch

--- folder 1/file1.py
+++ folder 1/file1.py
@@ -1,7 +1,7 @@
-print("Hello World")
+print("Hello World changed to hello prajal")
 
 def func1():
-    print("Func1 Master also changed in master")
+    print("Func1 Master rebased branch")
 
 def func_2():
     print("another func")
