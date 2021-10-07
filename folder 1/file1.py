abfdfc3dc133774daf90704b1f5374c1ad67e833
Pranjal <pranjalsrajput@gmail.com>
2021-10-07 10:36:05 +0200

    change in master

--- folder 1/file1.py
+++ folder 1/file1.py
@@ -1,7 +1,10 @@
 print("Hello World")
 
 def func1():
-    print("Func1 Master")
+    print("Func1 Master also changed in master")
 
 def func_2():
     print("another func")
+
+def func_3():
+    print("another func 4")
